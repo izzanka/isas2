c8b1912c908976b4b9a939954d8bf5cf83b0fa23
izzanka <izzancoc.46@gmail.com>
2022-05-27 20:06:25 +0700

    first commit

---
+++ src/selectionsortgame/SelectionSortGame.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package selectionsortgame;
+
+import main.Start;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SelectionSortGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Start user = new Start();
+        user.setLocationRelativeTo(null);
+        user.setVisible(true);
+    }
+    
+}
